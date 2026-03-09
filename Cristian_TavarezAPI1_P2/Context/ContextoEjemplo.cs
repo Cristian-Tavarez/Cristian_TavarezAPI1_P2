8061018784e492ebff6d7aff2510765a5f82dbf6
Cristian-Tavarez <139186025+Cristian-Tavarez@users.noreply.github.com>
2026-03-09 15:07:59 -0700

    Listo para Iniciar

---
+++ Cristian_TavarezAPI1_P2/Context/ContextoEjemplo.cs
@@ -0,0 +1,10 @@
+﻿using Cristian_TavarezAPI1_P2.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Cristian_TavarezAPI1_P2.Context;
+public class Contexto : DbContext
+{
+    public Contexto(DbContextOptions<Contexto> options) : base(options) { }
+
+    public DbSet<ViajesEspaciales> ViajesEspaciales { get; set; }
+}
